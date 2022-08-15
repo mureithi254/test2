dd190161f274372560747c0558e8f42ca630b0e3
james <lnewhenio@gmail.com>
2022-08-15 12:03:35 +0300

    first commit

---
+++ db/migrate/20220815084542_add_reset_password_sent_at_to_spree_users.solidus_auth.rb
@@ -0,0 +1,11 @@
+# This migration comes from solidus_auth (originally 20120203010234)
+# frozen_string_literal: true
+
+class AddResetPasswordSentAtToSpreeUsers < SolidusSupport::Migration[4.2]
+  def change
+    Spree::User.reset_column_information
+    unless Spree::User.column_names.include?("reset_password_sent_at")
+      add_column :spree_users, :reset_password_sent_at, :datetime
+    end
+  end
+end
